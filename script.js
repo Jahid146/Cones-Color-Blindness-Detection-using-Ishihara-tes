0f1db78db4806b0c2fae72ce41359eea88445522
jahid146 <jahidhassanrabbi1786@gmail.com>
2023-08-31 00:10:54 +0600

    This version has sign inmfeatures and all the modification needed for sign in and sign up and viewjs implementation.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { initializeApp } = require("firebase/app");
-const { getAuth, createUserWithEmailAndPassword, signInWithPopup, FacebookAuthProvider, GoogleAuthProvider, sendSignInLinkToEmail } = require("firebase/auth");
+const { getAuth, createUserWithEmailAndPassword, sendSignInLinkToEmail, signInWithEmailAndPassword } = require("firebase/auth");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 // Your Firebase configuration
 const firebaseConfig = {
@@ -21,8 +22,6 @@ const firebaseConfig = {
 
 const appFirebase = initializeApp(firebaseConfig);
 const auth = getAuth(appFirebase);
-
-
 
 // Handle POST request for user registration
 app.post("/sign-up", (req, res) => {
@@ -50,6 +49,26 @@ app.post("/sign-up", (req, res) => {
     });
 });
 
+// Handle POST request for user sign-in
+app.post("/sign-in", function (req, res) {
+  const email = req.body.email; // Change this to match the input field name
+  const password = req.body.password; // Change this to match the input field name
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("User signed in:", user.email); // Use user.email instead of user.email2
+      res.redirect("/"); // Redirect after successful sign-in
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Sign-in error:", errorCode, errorMessage);
+      res.redirect("/sign-in"); // Redirect with error message, if needed
+    });
+});
+
+
 function sendVerificationMail(email) {
   const action = {
     url: `http://localhost:3000/verify?email=${email}`,
@@ -65,39 +84,38 @@ function sendVerificationMail(email) {
     });
 }
 
-
-
-
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 });
 
 app.get("/check-email", (req, res) => {
-  res.sendFile(__dirname + "/check-email")
-})
+  res.render("check-email");
+});
 
 app.get("/sign-up", function (req, res) {
-  res.sendFile(__dirname + "/sign-up.html");
+  res.render("sign-up");
 });
 
 app.get("/sign-in", function (req, res) {
-  res.sendFile(__dirname + "/sign-in.html");
+  res.render("sign-in");
 });
 
 app.get("/About-us", function (req, res) {
-  res.sendFile(__dirname + "/About-us.html");
+  res.render("About-us");
 });
 
 app.get("/calculator", function (req, res) {
-  res.sendFile(__dirname + "/calculator.html");
+  res.render("calculator");
 });
+
 app.get("/verify-mail", function (req, res) {
-  res.sendFile(__dirname + "/verify-mail.html");
+  res.render("verify-mail");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 app.get("/verify", (req, res) => {
   // Extract the email from the query parameter
   const email = req.query.email;
